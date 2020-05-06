6858d3d1df0f5cde364e73776ab0c9371927200b
Yann Depps <yann.deperrot@protonmail.com>
2020-05-06 22:38:31 +0200

    spritesheet animation wip

--- JSSpriteAnimations/step_01/sketch.js
+++ JSSpriteAnimations/step_01/sketch.js
@@ -8,39 +8,105 @@ canvas.width = window.innerWidth;
 const images = [];
 images.player = new Image();
 images.player.src = './assets/Cuphead.png';
-const characterActions = ['up', 'top right', 'right', 'down right', 'down', 'jump'];
+// const characterActions = ['up', 'top right', 'right', 'down right', 'down', 'jump'];
+const characterActions = ['up', 'top right', 'right', 'jump', 'down right', 'down'];
+const numberOfCharacters = 10;
 const characters = [];
 
 // class
 class Character {
-  // see at 22.55
   constructor() {
     this.width = 103.0625;
     this.height = 113.125;
     this.frameX = 3;
-    this.frameY = 3;
-    this.x = 0;
-    this.y = 0;
-    this.speed = (Math.random() * 1.5) + 3.5;
-    this.action = 'right';
+    this.x = Math.random() * canvas.width - this.width;
+    this.y = Math.random() * canvas.height - this.height;
+    this.speed = (Math.random() * 2) + 3;
+    this.action = characterActions[Math.floor(Math.random() * characterActions.length)];
+    if ( this.action === 'up' ) {
+      this.frameY = 0;
+      this.minFrame = 4;
+      this.maxFrame = 15;
+    } else if (this.action === 'right') {
+      this.frameY = 3;
+      this.minFrame = 3;
+      this.maxFrame = 13;
+    } else if (this.action === 'jump') {
+      this.frameY = 7;
+      this.minFrame = 0;
+      this.maxFrame = 9;
+    } else if (this.action === 'down right') {
+      this.frameY = 4;
+      this.minFrame = 4;
+      this.maxFrame = 15;
+    } else if (this.action === 'top right') {
+      this.frameY = 1;
+      this.minFrame = 4;
+      this.maxFrame = 14;
+    } else if (this.action === 'down') {
+      this.frameY = 6;
+      this.minFrame = 0;
+      this.maxFrame = 12;
+    }
   }
   draw() {
     drawSprite(images.player, this.width*this.frameX, this.height*this.frameY,
                this.width, this.height,
                this.x, this.y, this.width, this.height);
     // animate sprites
-    if (this.frameX < 13) this.frameX++;
-    else this.frameX = 3;
+    if (this.frameX < this.maxFrame) this.frameX++;
+    else this.frameX = this.minFrame;
   }
   update() {
     if (this.action === 'right') {
-      if (this.x < canvas.width + this.width) this.x += this.speed;
-      else this.x = 0 - this.width;
+      if (this.x > canvas.width + this.width) {
+        this.x = 0 - this.width; 
+        this.y = Math.random() * (canvas.height - this.height);
+      }
+      else {
+        this.x += this.speed; 
+      }
+    } else if (this.action === 'up') {
+      if (this.y < (0 - this.height * 5)) {
+        this.y = canvas.height + this.height;
+        this.x = Math.random() * canvas.width;
+        this.speed = (Math.random() * 2) + 3;
+      } else {
+        this.y -= this.speed;
+      }
+    } else if (this.action === 'down right') {
+      if (this.y > canvas.height + this.height && this.x > this.width + canvas.width) {
+        this.y = 0 - this.height; 
+        this.x = Math.random() * canvas.width;
+        this.speed = (Math.random() *2) + 3;
+      } else {
+        this.y += this.speed;
+        this.x += this.speed;
+      }
+    } else if (this.action === 'top right') {
+      if (this.y < 0 - this.height && this.x > canvas.width + this.width) {
+        this.y = canvas.height + this.height;
+        this.x = Math.random() * canvas.width;
+        this.speed = (Math.random() * 2) + 3;
+      } else {
+        this.y -= this.speed;
+        this.x += this.speed;
+      }
+    } else if (this.action === 'down') {
+      if (this.y > canvas.height + (this.heght * 5)) {
+        this.y = 0 - this.height;
+        this.x = Math.random() * canvas.width;
+        this.speed = (Math.random() * 2) + 3;
+      } else {
+        this.y += this.speed;
+      }
     }
   }
 }
 
-characters.push(new Character());
+for (i = 0; i < numberOfCharacters; i++) {
+  characters.push(new Character());
+}
 
 // draw one frame from our spriteSheet
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
@@ -51,8 +117,10 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
 function animate() {
   // first clear the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  characters[0].draw();
-  characters[0].update();
+  for (i = 0; i < characters.length; i++) {
+    characters[i].draw();
+    characters[i].update();
+  }
 }
 
 window.onload = setInterval(animate, 1000/30);
