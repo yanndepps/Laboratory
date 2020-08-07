d44ca0e494a2376c32ab83e2134871081ad96de7
Yann Depps <yann.deperrot@protonmail.com>
2020-08-07 19:06:00 +0200

    clean up

--- Valentines/messages.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  `If I could give you one thing in life, I'd give you the ability to see yourself through my eyes, only then would you realize how special you are to me.`,
-  `If you were a movie, I'd watch you over and over again.`,
-  `What on earth did I think about all the time before you?`,
-  `If loving you was a job, I'd be the most deserving, dedicated, and qualified candidate. In fact, I'd even be willing to work for free!`,
-  `I know fairy tales come true because I have you.`,
-  `Whenever my phone vibrates, I hope you're the reason for it.`
-]
